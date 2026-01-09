498f247b72b13916682aefdd97d94f2a83415e80
pacter-69 <aldomcorb@gmail.com>
2026-01-09 21:37:29 +0100

    Scripts i carpeta
    Scripeta i carpits

---
+++ Assets/Scripts/LaneController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class LaneController
+{
+    static float[] laneValues = new float[] { 0f, 1.5f, 3f, 4.5f, 6f };
+
+    static public bool CanMove(int indexToMove, int currentIndex) {
+        return (currentIndex + indexToMove >= 0) && (currentIndex + indexToMove <= laneValues.Length - 1);
+    }
+}

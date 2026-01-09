851d97daca96c625dacf498bc7b349a5eba77b7a
ElBuenTocino <paumm8@gmail.com>
2026-01-09 23:02:14 +0100

    Car Movement
    Que el cotxe canvii de lane i vagi cap endavant

--- Assets/Scripts/LaneController.cs
+++ Assets/Scripts/LaneController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class LaneController
 {
-    static float[] laneValues = new float[] { 0f, 1.5f, 3f, 4.5f, 6f };
+    public static float[] laneValues = new float[] { 0f, 1.5f, 3f, 4.5f, 6f };
 
     static public bool CanMove(int indexToMove, int currentIndex) {
         return (currentIndex + indexToMove >= 0) && (currentIndex + indexToMove <= laneValues.Length - 1);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private int speed = 1;
+    public int speed = 1, speedUp = 2;
+    public int lane = 0;
+    
+    public InputActionReference upAction;
+    public InputActionReference leftAction;
+    public InputActionReference rightAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameObject.transform.position = new Vector2(LaneController.laneValues[lane], transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
-    
+
+    void Movement()
+    {
+        if (upAction.action.IsPressed())
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + speedUp * Time.deltaTime);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + speed * Time.deltaTime);
+        }
+
+        if (leftAction.action.WasPressedThisFrame())
+        {
+            if (LaneController.CanMove(-1, lane))
+            {
+                lane -= 1;
+                ChangeLane();
+            }
+        }
+        if (rightAction.action.WasPressedThisFrame())
+        {
+            if (LaneController.CanMove(1, lane))
+            {
+                lane += 1;
+                ChangeLane();
+            }
+        }
+    }
+    void ChangeLane()
+    {
+        gameObject.transform.position = new Vector2(LaneController.laneValues[lane], gameObject.transform.position.y);
+    }
 }

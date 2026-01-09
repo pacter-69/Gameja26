591e31b6cb8545addcce359b77d0ff6dec8a0693
ElBuenTocino <paumm8@gmail.com>
2026-01-09 23:24:24 +0100

    UI PACKET AHH

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -1,27 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GoalBehaviour : MonoBehaviour
 {
     public List<GameObject> cars;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if ((other.gameObject.CompareTag("Player")) || (other.gameObject.CompareTag("Car")))
         {
             cars.Add(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Player") && cars[1].CompareTag("Player"))
+        {
+            Debug.Log("WIN, player is second");
         }
     }
 }
---
+++ Assets/Scripts/SpotTracker.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotTracker : MonoBehaviour
+{
+    public List<GameObject> targets;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetSpot()
+    {
+        
+    }
+}

497b3e85f8bb51ebfce9f40bddc6a0ba9ff7db46
ElBuenTocino <paumm8@gmail.com>
2026-01-10 14:23:42 +0100

    credits

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -7,4 +7,25 @@ public class SceneHandler : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowPanel(GameObject panel)
+    {
+        if (panel.activeInHierarchy)
+        {
+            panel.SetActive(false);
+        }
+        else
+        {
+            panel.SetActive(true);
+        }
+    }
 }
